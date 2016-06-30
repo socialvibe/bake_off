0dacaeaf84951c5cd484c83ff5668c04ec27303e
Nathan Canning <nathancanning20@gmail.com>
2016-06-30 11:37:36 -0700

    Split up #{pie} and #{pie}.json routing
    kinda hacky for now, explanation in the comments

--- web/controllers/pie_controller.ex
+++ web/controllers/pie_controller.ex
@@ -6,6 +6,30 @@ defmodule BakeOff.PieController do
   end
 
   def show(conn, %{ "pie_id" => pie_id }) do
-    render conn, "show.html", pie_id: pie_id 
+    # there's almost definitely a better way to do this.
+    # unfortunately, pattern matching on string concatenation
+    # doesn't allow for the unkown bit to be at the beginning
+    # e.g. %{ "pie_id" => pie_id <> ".json" } won't work,
+    # and you can't use ends_with?/2 in a guard, e.g.
+    # def show(conn %{ "pie_id" => pie_id }) when ends_with?(pie_id, ".json") do
+    # anyway, this works for responding to pie_id and pie_id.json
+    # for now.
+    match = Regex.run(~r/(.+)\.json/, pie_id)
+    if is_nil(match) do
+      render conn, "show.html", pie_id: pie_id
+    else
+      [_|[id]] = match
+      json conn, as_json(id)
+    end
+  end
+
+  defp as_json(pie) do
+    %{
+      name: "pie #{pie}",
+      image_url: "http://imgur.com/#{pie}",
+      price_per_slice: "$1",
+      remaining_slices: 10,
+      purchases: [%{ user_name: "nathan", slices: 1 }]
+    }
   end
 end
