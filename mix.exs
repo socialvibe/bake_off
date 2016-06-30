015057ba6e161266f538822656fdeb42477decf3
Chris Johnson <findchris@gmail.com>
2016-06-30 11:56:53 -0700

    Remove Ecto and PubSub stuff, and add Redix for Redis access.

--- mix.exs
+++ mix.exs
@@ -18,8 +18,7 @@ defmodule BakeOff.Mixfile do
   # Type `mix help compile.app` for more information.
   def application do
     [mod: {BakeOff, []},
-     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+     applications: [:phoenix, :phoenix_html, :cowboy, :logger, :gettext, :redix]]
   end
 
   # Specifies which paths to compile per environment.
@@ -31,13 +30,11 @@ defmodule BakeOff.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [{:phoenix, "~> 1.2.0"},
-     {:phoenix_pubsub, "~> 1.0"},
-     {:phoenix_ecto, "~> 3.0"},
-     {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:redix, ">= 0.0.0"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
